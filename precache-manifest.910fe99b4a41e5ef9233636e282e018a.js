e5ea315890c4c6711e172ae96df5e428531e7f47
Jake Coppinger <jake@jakecoppinger.com>
2023-01-15 20:53:21 +1100

    Initial commit

---
+++ precache-manifest.910fe99b4a41e5ef9233636e282e018a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "20208f8b0f45e7e93f6746dfe03ded56",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b3cd5bc9fd1dbf59d9cf",
+    "url": "/static/css/main.8d462b44.chunk.css"
+  },
+  {
+    "revision": "0a9fc6b8b6f397f2d4d8",
+    "url": "/static/js/2.a748d332.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.a748d332.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b3cd5bc9fd1dbf59d9cf",
+    "url": "/static/js/main.cb2c05c7.chunk.js"
+  },
+  {
+    "revision": "26e317326c8b351a813c",
+    "url": "/static/js/runtime-main.48dd696b.js"
+  }
+]);
